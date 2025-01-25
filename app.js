b4c9220a16175bf983924aa8c7c6e1da6a147ef2
Magno <jmagno.dutra@hotmail.com>
2025-01-25 14:50:50 -0300

    Connected to DB

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const tasksRouter = require("./routes/tasksRoute");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
 
@@ -16,4 +18,13 @@ app.use("/api/v1/tasks", tasksRouter);
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}...`));
+async function start() {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,21 +1,21 @@
 function getAllTasks(req, res) {
-  res.json({ success: true, data: [] });
+  res.json(req.body);
 }
 
 function getTask(req, res) {
-  res.send("get single task");
+  res.json({ id: req.params.id });
 }
 
 function createTask(req, res) {
-  res.send("create single task");
+  res.json(req.body);
 }
 
 function updateTask(req, res) {
-  res.send("update single task");
+  res.json({ id: req.params.id });
 }
 
 function deleteTask(req, res) {
-  res.send("delete single task");
+  res.json({ id: req.params.id });
 }
 
 module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
