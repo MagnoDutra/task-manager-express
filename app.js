491028071321b7f0ef61375cbdf1b8496824b5a8
Magno <jmagno.dutra@hotmail.com>
2025-01-25 01:34:43 -0300

    Basic express server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/hello", (req, res) => {
+  res.send("<h1>Task Manager App</h1>");
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}...`));
