e610b5ec4c76cb3d983cde482da1feb2ff021e13
Magno <jmagno.dutra@hotmail.com>
2025-01-26 21:14:49 -0300

    Get endpoint created

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,16 +1,35 @@
 const Task = require("../models/taskModel");
 
-function getAllTasks(req, res) {
-  res.json(req.body);
+async function getAllTasks(req, res) {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
-function getTask(req, res) {
-  res.json({ id: req.params.id });
+async function getTask(req, res) {
+  const { id } = req.params;
+
+  try {
+    const singleTask = await Task.findById(id);
+
+    if (!singleTask) return res.status(404).json({ msg: "n encontrado" });
+
+    res.status(200).json({ task: singleTask });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
 async function createTask(req, res) {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 }
 
 function updateTask(req, res) {
