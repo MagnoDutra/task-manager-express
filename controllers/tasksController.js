2aaed44f686de17439960759d258bb684f851f45
Magno <jmagno.dutra@hotmail.com>
2025-01-28 09:13:20 -0300

    Wrapper for try catch created

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,73 +1,53 @@
 const Task = require("../models/taskModel");
+const asyncWrapper = require("../middlewares/async-wrapper");
 
-async function getAllTasks(req, res) {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-async function getTask(req, res) {
+const getTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
+  const singleTask = await Task.findById(id);
 
-  try {
-    const singleTask = await Task.findById(id);
+  if (!singleTask) return res.status(404).json({ message: "n encontrado" });
 
-    if (!singleTask) return res.status(404).json({ message: "n encontrado" });
+  res.status(200).json({ task: singleTask });
+});
 
-    res.status(200).json({ task: singleTask });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-async function createTask(req, res) {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ message: error });
-  }
-}
-
-async function updateTask(req, res) {
+const updateTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
   const data = req.body;
 
-  try {
-    const task = await Task.findOneAndUpdate({ _id: id }, data, {
-      new: true,
-      runValidators: true,
-    });
+  const task = await Task.findOneAndUpdate({ _id: id }, data, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!task) {
-      return res.status(404).json({ message: "couldnt find with this id" });
-    }
-
-    return res.status(200).json({ task });
-  } catch (error) {
-    return res.status(500).json({ error });
+  if (!task) {
+    return res.status(404).json({ message: "couldnt find with this id" });
   }
-}
 
-async function deleteTask(req, res) {
+  return res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const task = await Task.findByIdAndDelete({ _id: id });
+  const task = await Task.findByIdAndDelete({ _id: id });
 
-    if (task !== null) {
-      return res.status(200).json({ success: true, data: task });
-    } else {
-      return res
-        .status(404)
-        .json({ success: false, message: "Couldnt find with this id" });
-    }
-  } catch (error) {
-    return res.status(500).json({ success: false, error });
+  if (task !== null) {
+    return res.status(200).json({ success: true, data: task });
+  } else {
+    return res
+      .status(404)
+      .json({ success: false, message: "Couldnt find with this id" });
   }
-}
+});
 
 module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
