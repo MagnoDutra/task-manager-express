dfba1ac2d69b4d219a681dd2a38d1e77ce0531cc
Magno <jmagno.dutra@hotmail.com>
2025-01-28 09:49:02 -0300

    Error handling done

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,16 +1,19 @@
 const Task = require("../models/taskModel");
 const asyncWrapper = require("../middlewares/async-wrapper");
+const { createCustomError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
   const singleTask = await Task.findById(id);
 
-  if (!singleTask) return res.status(404).json({ message: "n encontrado" });
+  if (!singleTask) {
+    return next(createCustomError("Task not found", 404));
+  }
 
   res.status(200).json({ task: singleTask });
 });
@@ -20,7 +23,7 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
   const data = req.body;
 
@@ -30,13 +33,13 @@ const updateTask = asyncWrapper(async (req, res) => {
   });
 
   if (!task) {
-    return res.status(404).json({ message: "couldnt find with this id" });
+    return next(createCustomError("Task not found", 404));
   }
 
   return res.status(200).json({ task });
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id } = req.params;
 
   const task = await Task.findByIdAndDelete({ _id: id });
@@ -44,9 +47,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   if (task !== null) {
     return res.status(200).json({ success: true, data: task });
   } else {
-    return res
-      .status(404)
-      .json({ success: false, message: "Couldnt find with this id" });
+    return next(createCustomError("Task not found", 404));
   }
 });
 
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,11 @@
+const { CustomAPIError } = require("../errors/custom-error");
+
 function errorHandlerMiddleware(err, req, res, next) {
-  return res.status(500).json({ err });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+
+  return res.status(500).json({ message: "Something went wrong" });
 }
 
 module.exports = errorHandlerMiddleware;
